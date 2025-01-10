8bb7c8a9f62987e883348b5d2a7cb7386bdce124
missarii <missariahil10@gmail.com>
2025-01-10 17:20:01 +0530

    refactor(config): decouple notification service

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,3 +1,5 @@
 # frontend/src/hooks/useAuth.ts
 
 # 2025-01-01T13:07:49 - perf(config): reduce bundle size with tree shaking
+
+# 2025-01-10T17:20:01 - refactor(config): decouple notification service
