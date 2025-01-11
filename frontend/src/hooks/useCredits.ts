5bce6abbad6dde63ba50f1a3d25905c19537e568
missarii <missariahil10@gmail.com>
2025-01-11 16:19:29 +0530

    test: add performance benchmark tests

--- frontend/src/hooks/useCredits.ts
+++ frontend/src/hooks/useCredits.ts
@@ -3,3 +3,5 @@
 # 2025-01-02T16:48:34 - test: add performance benchmark tests
 
 # 2025-01-07T12:49:51 - feat: introduce rate limiting middleware
+
+# 2025-01-11T16:19:29 - test: add performance benchmark tests
