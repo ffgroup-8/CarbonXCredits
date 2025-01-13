2442e4ff2b218d2fca7a8fdafe18e36016152564
missarii <missariahil10@gmail.com>
2025-01-13 09:50:44 +0530

    refactor: abstract api client into shared module

--- src/core/audit.py
+++ src/core/audit.py
@@ -3,3 +3,5 @@
 # 2025-01-07T14:36:22 - chore: update dependencies to latest versions
 
 # 2025-01-09T16:13:37 - feat(ui): add user profile settings page
+
+# 2025-01-13T09:50:44 - refactor: abstract api client into shared module
