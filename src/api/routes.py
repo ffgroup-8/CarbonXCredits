30f6fc2635fcd300b34e4eb01f81fe990881d448
missarii <missariahil10@gmail.com>
2025-01-16 17:59:42 +0530

    fix: patch authentication token refresh bug

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,3 +3,5 @@
 # 2025-01-03T17:21:55 - feat: add multi-language support
 
 # 2025-01-08T09:42:13 - feat(ui): add multi-language support
+
+# 2025-01-16T17:59:42 - fix: patch authentication token refresh bug
