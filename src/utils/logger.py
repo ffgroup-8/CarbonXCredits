539610caf7fad039ec6c1d36d403edff63898405
missarii <missariahil10@gmail.com>
2025-01-21 17:25:45 +0530

    docs(config): write setup guide for new developers

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,3 +5,5 @@
 # 2025-01-07T11:38:51 - test: add regression test suite
 
 # 2025-01-13T13:24:38 - test: write test cases for edge conditions
+
+# 2025-01-21T17:25:45 - docs(config): write setup guide for new developers
