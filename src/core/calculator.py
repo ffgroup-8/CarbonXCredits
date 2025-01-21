185640f2b2fb28ef017015a2c8f2ffb8e3529745
missarii <missariahil10@gmail.com>
2025-01-21 13:36:27 +0530

    test: add snapshot tests for components

--- src/core/calculator.py
+++ src/core/calculator.py
@@ -3,3 +3,5 @@
 # 2025-01-04T10:10:11 - test: improve test coverage to 85%
 
 # 2025-01-17T08:09:10 - style: consolidate css utility classes
+
+# 2025-01-21T13:36:27 - test: add snapshot tests for components
