2cc9fb65385a91019d384a30dcb974dcaf07af3f
missarii <missariahil10@gmail.com>
2025-01-23 12:29:28 +0530

    chore: set up eslint and prettier config

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,3 +3,5 @@
 # 2025-01-10T10:16:03 - fix(db): patch authentication token refresh bug
 
 # 2025-01-18T18:37:31 - style(auth): format code with prettier
+
+# 2025-01-23T12:29:28 - chore: set up eslint and prettier config
