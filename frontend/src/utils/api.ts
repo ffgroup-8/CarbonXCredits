38b54efd69ac37c39180ee3cbbd824b03177f080
missarii <missariahil10@gmail.com>
2025-01-24 13:52:08 +0530

    fix: resolve slow query on projects index

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -5,3 +5,5 @@
 # 2025-01-18T18:37:31 - style(auth): format code with prettier
 
 # 2025-01-23T12:29:28 - chore: set up eslint and prettier config
+
+# 2025-01-24T13:52:08 - fix: resolve slow query on projects index
