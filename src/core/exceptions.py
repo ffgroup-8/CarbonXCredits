a79e7743e41cb711468aef943325371bd790b037
missarii <missariahil10@gmail.com>
2025-01-28 15:57:53 +0530

    style: format code with prettier

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -3,3 +3,5 @@
 # 2025-01-07T17:18:44 - test: mock external services in tests
 
 # 2025-01-25T15:12:14 - chore(api): bump version to 1.2.3
+
+# 2025-01-28T15:57:53 - style: format code with prettier
