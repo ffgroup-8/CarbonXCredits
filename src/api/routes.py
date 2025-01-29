828831d1879d4441fc484854e1812ded4a1c721f
missarii <missariahil10@gmail.com>
2025-01-29 16:09:53 +0530

    docs: add jsdoc comments to core modules

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,3 +5,5 @@
 # 2025-01-08T09:42:13 - feat(ui): add multi-language support
 
 # 2025-01-16T17:59:42 - fix: patch authentication token refresh bug
+
+# 2025-01-29T16:09:53 - docs: add jsdoc comments to core modules
