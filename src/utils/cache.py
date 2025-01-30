29c53e402b04250a5d11df231bac2378dc4afb10
missarii <missariahil10@gmail.com>
2025-01-30 12:58:53 +0530

    style(db): fix indentation across project

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -7,3 +7,5 @@
 # 2025-01-21T13:10:51 - test: improve test coverage to 85%
 
 # 2025-01-29T14:05:58 - test: write integration tests for api
+
+# 2025-01-30T12:58:53 - style(db): fix indentation across project
