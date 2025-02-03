4f8188cbeeb1351504d1f27eb08d4da68584369e
missarii <missariahil10@gmail.com>
2025-02-03 15:44:31 +0530

    perf(api): add indexes to frequently queried columns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,3 +3,5 @@
 # 2025-01-03T18:09:04 - docs: create architecture overview diagram
 
 # 2025-01-23T13:38:30 - test: add performance benchmark tests
+
+# 2025-02-03T15:44:31 - perf(api): add indexes to frequently queried columns
