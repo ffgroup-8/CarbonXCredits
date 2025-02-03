5f3b8b80519dcc6de0f8279334e42511565146b9
missarii <missariahil10@gmail.com>
2025-02-03 09:27:38 +0530

    perf: optimize image loading with lazy loading

--- frontend/src/components/CreditForm.tsx
+++ frontend/src/components/CreditForm.tsx
@@ -1,3 +1,5 @@
 # frontend/src/components/CreditForm.tsx
 
 # 2025-01-06T15:16:42 - style: add hover states to interactive elements
+
+# 2025-02-03T09:27:38 - perf: optimize image loading with lazy loading
