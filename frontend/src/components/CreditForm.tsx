836adea977aa8df3627560f3cecf575ad480b66c
missarii <missariahil10@gmail.com>
2025-02-03 09:46:27 +0530

    test: add snapshot tests for components

--- frontend/src/components/CreditForm.tsx
+++ frontend/src/components/CreditForm.tsx
@@ -3,3 +3,5 @@
 # 2025-01-06T15:16:42 - style: add hover states to interactive elements
 
 # 2025-02-03T09:27:38 - perf: optimize image loading with lazy loading
+
+# 2025-02-03T09:46:27 - test: add snapshot tests for components
