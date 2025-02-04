9656c56e0aa7e0f993baae5c0488bc71e126963d
missarii <missariahil10@gmail.com>
2025-02-04 09:12:50 +0530

    refactor(auth): optimize database query patterns

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -5,3 +5,5 @@
 # 2025-01-25T15:12:14 - chore(api): bump version to 1.2.3
 
 # 2025-01-28T15:57:53 - style: format code with prettier
+
+# 2025-02-04T09:12:50 - refactor(auth): optimize database query patterns
