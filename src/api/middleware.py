15fe857fb10948eb5d6615bf69788f27840d421e
missarii <missariahil10@gmail.com>
2025-02-04 19:45:59 +0530

    style: add hover states to interactive elements

--- src/api/middleware.py
+++ src/api/middleware.py
@@ -3,3 +3,5 @@
 # 2025-01-12T13:38:09 - chore: add .gitignore for ide files
 
 # 2025-01-16T17:39:43 - chore: update license year
+
+# 2025-02-04T19:45:59 - style: add hover states to interactive elements
