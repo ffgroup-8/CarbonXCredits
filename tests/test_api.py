3706d102e6c31a9e5cf88a42ded0ac9f0dba1026
missarii <missariahil10@gmail.com>
2025-02-05 17:04:08 +0530

    chore: configure ci/cd pipeline

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,3 +3,5 @@
 # 2025-01-14T09:27:06 - docs: add jsdoc comments to core modules
 
 # 2025-01-22T18:52:09 - feat: introduce caching layer for api
+
+# 2025-02-05T17:04:08 - chore: configure ci/cd pipeline
