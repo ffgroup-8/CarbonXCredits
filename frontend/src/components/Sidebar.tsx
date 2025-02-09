2d2b1da769b557f085e59bd2009b112e267911ac
missarii <missariahil10@gmail.com>
2025-02-09 15:01:05 +0530

    perf(core): add indexes to frequently queried columns

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,3 +3,5 @@
 # 2025-01-13T16:18:29 - test: write integration tests for api
 
 # 2025-02-03T11:02:50 - docs: add changelog entry for release
+
+# 2025-02-09T15:01:05 - perf(core): add indexes to frequently queried columns
