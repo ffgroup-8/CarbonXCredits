b319c4656485113b55f94a7bd188ccc54ef44f1c
missarii <missariahil10@gmail.com>
2025-02-11 16:53:32 +0530

    perf: reduce api response payload size

--- src/db/models.py
+++ src/db/models.py
@@ -3,3 +3,5 @@
 # 2025-01-16T16:49:52 - chore: set up monitoring and alerting
 
 # 2025-01-24T19:43:58 - refactor: replace class components with functional
+
+# 2025-02-11T16:53:32 - perf: reduce api response payload size
