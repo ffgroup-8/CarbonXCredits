29dc7c8842237b4c15bbc94c30e8dc39eb294927
missarii <missariahil10@gmail.com>
2025-02-14 11:03:06 +0530

    chore: configure docker compose for dev

--- src/main.py
+++ src/main.py
@@ -3,3 +3,5 @@
 # 2025-01-01T13:42:06 - test(api): write integration tests for api
 
 # 2025-01-30T10:08:02 - fix(ui): fix calculation rounding error
+
+# 2025-02-14T11:03:06 - chore: configure docker compose for dev
