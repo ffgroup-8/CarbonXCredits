9b8dc44beb0a4276969b142fd42dc3b3dd33fdb6
missarii <missariahil10@gmail.com>
2025-02-15 11:34:47 +0530

    feat: add file upload component

--- src/db/models.py
+++ src/db/models.py
@@ -5,3 +5,5 @@
 # 2025-01-24T19:43:58 - refactor: replace class components with functional
 
 # 2025-02-11T16:53:32 - perf: reduce api response payload size
+
+# 2025-02-15T11:34:47 - feat: add file upload component
