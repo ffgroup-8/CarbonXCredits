d037a95e935805e96987167676ad3a18c700c449
missarii <missariahil10@gmail.com>
2025-03-02 10:08:46 +0530

    perf: improve database query performance

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -3,3 +3,5 @@
 # 2025-01-31T17:26:11 - feat: add file upload component
 
 # 2025-02-12T13:30:22 - test: add performance benchmark tests
+
+# 2025-03-02T10:08:46 - perf: improve database query performance
