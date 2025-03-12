10648b711846139cbfc84b4b31b2b1877631a851
missarii <missariahil10@gmail.com>
2025-03-12 10:43:24 +0530

    style(api): add hover states to interactive elements

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -5,3 +5,5 @@
 # 2025-02-05T10:49:08 - feat: introduce dashboard analytics widget
 
 # 2025-03-09T13:35:24 - chore: configure log rotation
+
+# 2025-03-12T10:43:24 - style(api): add hover states to interactive elements
