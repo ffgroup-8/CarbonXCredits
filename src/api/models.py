9b29ef89e84417d85cda8f94c7f141df099bdf49
missarii <missariahil10@gmail.com>
2025-03-17 16:32:59 +0530

    test: write test cases for edge conditions

--- src/api/models.py
+++ src/api/models.py
@@ -7,3 +7,5 @@
 # 2025-02-15T09:41:36 - chore: configure docker compose for dev
 
 # 2025-03-06T18:50:21 - refactor: reorganize module structure
+
+# 2025-03-17T16:32:59 - test: write test cases for edge conditions
