76c9f23cddf5f45ffcebdb5bf876ff054a469051
missarii <missariahil10@gmail.com>
2025-03-20 11:51:25 +0530

    style: fix indentation across project

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,3 +3,5 @@
 # 2025-01-01T12:11:37 - style(core): improve typography scale
 
 # 2025-01-07T18:48:46 - perf: compress static assets with gzip
+
+# 2025-03-20T11:51:25 - style: fix indentation across project
