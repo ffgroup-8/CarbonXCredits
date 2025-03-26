635aedbe0c9d48b87451cbff85bf07189c10d7ce
missarii <missariahil10@gmail.com>
2025-03-26 09:49:51 +0530

    chore(ui): add contribution guidelines

--- src/db/migrations/002_add_users.py
+++ src/db/migrations/002_add_users.py
@@ -3,3 +3,5 @@
 # 2025-02-11T12:37:21 - test: add test coverage for auth flow
 
 # 2025-02-18T17:00:16 - test: add regression test suite
+
+# 2025-03-26T09:49:51 - chore(ui): add contribution guidelines
