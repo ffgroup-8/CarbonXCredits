7f66983d1c86853fbe4fe08505d3b8e6604aac8f
missarii <missariahil10@gmail.com>
2025-04-09 12:49:54 +0530

    perf: add indexes to frequently queried columns

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,3 +7,5 @@
 # 2025-02-05T17:04:08 - chore: configure ci/cd pipeline
 
 # 2025-03-07T08:11:59 - chore: add contribution guidelines
+
+# 2025-04-09T12:49:54 - perf: add indexes to frequently queried columns
