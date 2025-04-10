a1916b3711e055590069c08d862ed461eb10d530
missarii <missariahil10@gmail.com>
2025-04-10 09:13:33 +0530

    chore(api): update license year

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -3,3 +3,5 @@
 # 2025-01-01T10:21:26 - refactor: streamline error handling middleware
 
 # 2025-02-24T16:13:48 - chore: configure log rotation
+
+# 2025-04-10T09:13:33 - chore(api): update license year
