27952e35fa44bb7a558a05d424cbc37cfdb78480
missarii <missariahil10@gmail.com>
2025-04-14 15:51:08 +0530

    chore(ui): set up eslint and prettier config

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -17,3 +17,5 @@
 # 2025-03-31T08:00:44 - chore(db): update package-lock.json
 
 # 2025-04-05T08:21:51 - style(config): improve typography scale
+
+# 2025-04-14T15:51:08 - chore(ui): set up eslint and prettier config
