94bec013d14beff936347502320d6354ac79dc5f
missarii <missariahil10@gmail.com>
2025-05-04 15:37:35 +0530

    chore: set up staging environment

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -13,3 +13,5 @@
 # 2025-04-23T13:13:12 - test: improve test coverage to 85%
 
 # 2025-04-30T10:39:38 - perf: implement virtual scrolling for lists
+
+# 2025-05-04T15:37:35 - chore: set up staging environment
