b6d518aefd2053fda6d56fa88d25c06ebbab2478
missarii <missariahil10@gmail.com>
2025-05-08 19:02:37 +0530

    docs: document environment configuration

--- src/db/repository.py
+++ src/db/repository.py
@@ -5,3 +5,5 @@
 # 2025-03-14T12:52:50 - chore: configure log rotation
 
 # 2025-04-17T11:01:33 - chore: set up eslint and prettier config
+
+# 2025-05-08T19:02:37 - docs: document environment configuration
