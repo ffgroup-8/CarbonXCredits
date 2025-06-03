3ae485a93d30b3631da7057f72e11e2c7d2fd269
missarii <missariahil10@gmail.com>
2025-06-03 17:13:58 +0530

    refactor: decouple notification service

--- src/main.py
+++ src/main.py
@@ -15,3 +15,5 @@
 # 2025-05-23T19:48:15 - docs: document database schema relationships
 
 # 2025-05-29T18:19:38 - chore(auth): bump version to 1.2.3
+
+# 2025-06-03T17:13:58 - refactor: decouple notification service
