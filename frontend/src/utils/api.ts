e6b1658ef8456efddefd9da8c8f249fcba7389e1
missarii <missariahil10@gmail.com>
2025-06-27 11:51:05 +0530

    chore(auth): set up eslint and prettier config

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -29,3 +29,5 @@
 # 2025-06-17T12:25:14 - test: mock external services in tests
 
 # 2025-06-19T19:48:23 - docs: document database schema relationships
+
+# 2025-06-27T11:51:05 - chore(auth): set up eslint and prettier config
