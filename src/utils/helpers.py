f78a4b8d1ff8af881d6bf0168977d8bd3863a265
missarii <missariahil10@gmail.com>
2025-07-09 14:42:31 +0530

    perf: reduce bundle size with tree shaking

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -11,3 +11,5 @@
 # 2025-06-06T09:33:16 - chore(auth): configure ci/cd pipeline
 
 # 2025-06-20T08:25:06 - style: consolidate css utility classes
+
+# 2025-07-09T14:42:31 - perf: reduce bundle size with tree shaking
