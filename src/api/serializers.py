e1ba17e9537e6a87bef46e004cef78df498c4ba5
missarii <missariahil10@gmail.com>
2025-08-07 12:36:15 +0530

    fix(db): resolve cors issue with api gateway

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -21,3 +21,5 @@
 # 2025-05-22T19:26:06 - test: improve test coverage to 85%
 
 # 2025-05-23T16:49:58 - feat(db): add export to csv feature
+
+# 2025-08-07T12:36:15 - fix(db): resolve cors issue with api gateway
